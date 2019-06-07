6cc4c916b89f51379f7db68dcdf605518d2221e1
Aweys Ahmed <aweys.ahmed@outlook.com>
2019-06-06 21:55:34 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,6 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == 
       redirect_to(root_url) unless current_user?(@user)
     end
 end
